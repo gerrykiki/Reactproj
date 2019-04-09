1acb68767ea1d72b4b95cc4bec9850ab0ccd7037
Gerry PW Lin <gerry_pw_lin@wistron.com>
2019-04-09 15:25:53 +0800

    Updates

---
+++ precache-manifest.c3e9a605e3f2a19dd3b7954bdc254a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reactproj/myfirstweb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb4eff76670ef9f650e7",
+    "url": "/Reactproj/myfirstweb/static/js/runtime~main.6a4673a3.js"
+  },
+  {
+    "revision": "2b4ef7f7f3bfc19103f9",
+    "url": "/Reactproj/myfirstweb/static/js/main.c241ee20.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Reactproj/myfirstweb/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2b4ef7f7f3bfc19103f9",
+    "url": "/Reactproj/myfirstweb/static/css/main.bac6af2f.chunk.css"
+  },
+  {
+    "revision": "67a36c0f097718fd12145d02f61cd213",
+    "url": "/Reactproj/myfirstweb/index.html"
+  }
+];
