5b4bbd76c6e4e50431c076c2427b78b77b06389a
Gerry PW Lin <gerry_pw_lin@wistron.com>
2019-07-12 14:30:09 +0800

    Commit receive and  sender code

---
+++ precache-manifest.1bd84d217915db0e90c35204e10630fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a108dd5221917f7cd0f19a73c987f664",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be896e68ea677c880e66",
+    "url": "./static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "a8ab3f692619886ca0f3",
+    "url": "./static/css/main.ab692817.chunk.css"
+  },
+  {
+    "revision": "be896e68ea677c880e66",
+    "url": "./static/js/2.c325579f.chunk.js"
+  },
+  {
+    "revision": "a8ab3f692619886ca0f3",
+    "url": "./static/js/main.7c3d9133.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f527caeeb50467b474b6b0b518536864.js"
+  "./precache-manifest.1bd84d217915db0e90c35204e10630fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
